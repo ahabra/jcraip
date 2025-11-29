a3def9ba74f8da751d743cfe1a78c49ac4e99956
Abdul Habra <abdul.habra@gmail.com>
2025-11-28 20:56:45 -0500

    first push

---
+++ README.md
@@ -0,0 +1,6 @@
+# Java Code Retrieval Ai Prompt (jcrap) 
+Created on Fri Nov 28 20:28:18 EST 2025
+
+
+
+
---
+++ src/main/java/com/tek271/jcrap/App.java
@@ -0,0 +1,10 @@
+package com.tek271.jcrap;
+
+/**
+ * Hello world!
+ */
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
