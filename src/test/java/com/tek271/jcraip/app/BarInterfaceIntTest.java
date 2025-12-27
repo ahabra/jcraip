6ef76ee3e8cd3d21ad9cf203cce8b617b92c5bb2
Abdul Habra <abdul.habra@gmail.com>
2025-12-27 16:51:25 -0500

    put integration test in own file

---
+++ src/test/java/com/tek271/jcraip/app/BarInterfaceIntTest.java
@@ -0,0 +1,21 @@
+package com.tek271.jcraip.app;
+
+import com.tek271.jcraip.ai.base.AiCaller;
+import com.tek271.jcraip.ai.gemini.GeminiCaller;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled("running this often will cost $")
+class BarInterfaceIntTest {
+  AiCaller aiCaller = new GeminiCaller();
+  BarInterface sut = BarInterface.create(aiCaller);
+
+  @Test
+  void willRunMethodWithPrompt() {
+    int max = sut.max(13, 90, 3);
+    assertEquals(90, max);
+  }
+
+}
